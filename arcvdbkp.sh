ec23bbcf2549f145548f089b193ac23dc6dc766f
nicholsonz <77360596+nicholsonz@users.noreply.github.com>
2021-02-12 00:30:01 -0600

    Update arcvdbkp.sh

--- arcvdbkp.sh
+++ arcvdbkp.sh
@@ -3,24 +3,17 @@
 # Archive Backup Script
 
 BACKUP_DIR="/mnt/backup/$(hostname)/arcvdbkp"
-
-TODAY=$(date +"%m-%d-%Y")
-DAILY_DELETE_NAME=`date +"%a-%m-%d-%Y" --date '7 days ago'`
-WEEKLY_DELETE_NAME="week"`date +"%W-%Y" --date '5 weeks ago'`
-MONTHLY_DELETE_NAME=`date +"%B-%Y" --date '12 months ago'`
-DAY=$(date +"%a")
-
-
+TODAY=$(date +"%a-%m-%d-%Y")
 LOGFILE="$BACKUP_DIR/arcvdbkp.log"
-
-
 BACKUP_FILES="/home /etc"
 
-if [ ! -d $BACKUP_DIR ]; then
+if [ -d $BACKUP_DIR ]; then
+  echo "Backup directory exists... Backing up dirs/files now."
+ elif [ ! -d $BACKUP_DIR ]; then
   echo "Creating backup directory."
   mkdir $BACKUP_DIR
 else
-  echo "Error.  Cannot create backup dir."
+  echo "Error!  Cannot create backup dir. Check dir permissions or backup drive availability."
 
 fi
 
@@ -29,13 +22,13 @@ echo "  Archive Backup Start! $(date)"
 echo "####################################################"
 
 
-echo "Backing up $BACKUP_FILES to $BACKUP_DIR/$DAY-$TODAY.tgz"
+echo "Backing up $BACKUP_FILES to $BACKUP_DIR/$TODAY.tgz"
 echo
 
 
-if [ ! -e $BACKUP_DIR/$DAY-$TODAY.tgz ]; then
-        tar czp --exclude="*[Cc]ache*" --exclude="[Tt]rash" --exclude="*[Ss]team" --exclude="$BACKUP_DIR" --exclude="/home/*/Downloads" -f $BACKUP_DIR/$DAY-$TODAY.tgz $BACKUP_FILES
-   else
+if [ ! -e $BACKUP_DIR/$TODAY.tgz ]; then
+        tar czp --exclude="*[Cc]ache*" --exclude="[Tt]rash" --exclude="*[Ss]team" --exclude="$BACKUP_DIR" --exclude="/home/*/Downloads" -f $BACKUP_DIR/$TODAY.tgz $BACKUP_FILES  
+    else
       echo "Daily backup already exists."
 fi
 
@@ -45,7 +38,7 @@ DAY_NUM=$(date +%u)
 MONTH=$(date +%B-%Y)
   if [ ! -e $BACKUP_DIR/$MONTH.tgz ]; then
      echo "Making monthly backup of $MONTH"
-    cp $BACKUP_DIR/$DAY-$TODAY.tgz $BACKUP_DIR/$MONTH.tgz
+    cp $BACKUP_DIR/$TODAY.tgz $BACKUP_DIR/$MONTH.tgz
 
   else 
      echo "No need for monthly backup at this time."  
@@ -53,53 +46,41 @@ MONTH=$(date +%B-%Y)
 
 
 echo 
-echo "*** Delete Old Backups ***"
-echo
-echo "To be deleted if present:"
-echo " $DAILY_DELETE_NAME"
-#echo " $WEEKLY_DELETE_NAME"
-echo " $MONTHLY_DELETE_NAME"
+echo "*** Delete Aging Backups ***"
+echo "****************************"
 echo
 
-# delete old backups
-  if [ -e "$BACKUP_DIR/$DAILY_DELETE_NAME.tgz" ]; then
-        echo "   Deleting $BACKUP_DIR/$DAILY_DELETE_NAME.tgz"
-        rm -rf $BACKUP_DIR/$DAILY_DELETE_NAME.tgz
-  fi
+WKOLD_DAY=$(date +"%a-%m-%d-%Y" --date="last week")
+find $BACKUP_DIR -type f -name "$WKOLD_DAY*.tgz" | while read fname; do
+  echo "Deleting $fname..."
+  rm -rf "$fname"
+done
 
-  if [ -e "$BACKUP_DIR/$MONTHLY_DELETE_NAME.tgz" ]; then
-        echo "   Deleting $BACKUP_DIR/$MONTHLY_DELETE_NAME.tgz"
-    rm -rf $BACKUP_DIR/$MONTHLY_DELETE_NAME.tgz
-  fi
-
-echo
-echo "**************************"
-echo
-
-DAY_NAME=$(date +"%A")
-echo "Cleaning up old $DAY_NAME backups"
-find $BACKUP_DIR -name "$DAY*" -type f -mtime +8 -exec rm -f {} \;
+YROLD_MNTH=$(date +%B-%Y --date="last year")
+find $BACKUP_DIR -type f -name "$YROLD_MNTH*.tgz" | while read fname; do
+  echo "Deleting $fname..."
+  rm -rf "$fname"
+done
 
 echo
-echo "###### Starting backup ######"
+echo "*** Finished Removing Aged Backups ***"  
+echo "**************************************"
 echo
-
         
-        echo
-        echo "##########  Directory Listing  ###########"
-        echo
+echo
+echo "##########  Directory Listing  ###########"
+echo
 
 ls -lh $BACKUP_DIR/
 
 if test -e "$BACKUP_DIR/arcvdbkp.log"; then
-   	echo "$(date) Success!" >> "$BACKUP_DIR/arcvdbkp.log"    
-        echo
-	echo "####################################################"
-	echo "  Archived Backup Completed! $(date)"
-	echo "####################################################"
+ echo "$(date) Success!" >> "$BACKUP_DIR/arcvdbkp.log"    
+ echo
+ echo "####################################################"
+ echo "    Archived Backup Completed! $(date)"
+ echo "####################################################" 
 
 else
 	touch "$BACKUP_DIR/arcvdbkp.log"
 	echo "New log file created!"
 fi
-
